4ad86f80c2be01c0cd2dab6f4f36cba1e650a3b4
Carl Beekhuizen <carl@ethereum.org>
2020-02-18 18:40:28 +0100

    Seperate out tests per language

--- src/tests/test_key_derivation/test_mnemonic.py
+++ src/tests/test_key_derivation/test_mnemonic.py
@@ -1,3 +1,4 @@
+import pytest
 from json import load
 from key_derivation.mnemonic import (
     get_seed,
@@ -7,13 +8,15 @@ from key_derivation.mnemonic import (
 with open('tests/test_key_derivation/test_vectors/mnemonic.json', 'r') as f:
     test_vectors = load(f)
 
+@pytest.mark.parametrize(
+    'language,language_test_vectors',
+    [(a, b) for a, b in test_vectors.items()]
+)
+def test_bip39(language, language_test_vectors):
+    for test in language_test_vectors:
+        test_entropy = bytes.fromhex(test[0])
+        test_mnemonic = test[1]
+        test_seed = bytes.fromhex(test[2])
 
-def test_bip39():
-    for language, language_test_vectors in test_vectors.items():
-        for test in language_test_vectors:
-            test_entropy = bytes.fromhex(test[0])
-            test_mnemonic = test[1]
-            test_seed = bytes.fromhex(test[2])
-
-            assert get_mnemonic(language=language, entropy=test_entropy) == test_mnemonic
-            assert get_seed(mnemonic=test_mnemonic, password='TREZOR') == test_seed
+        assert get_mnemonic(language=language, entropy=test_entropy) == test_mnemonic
+        assert get_seed(mnemonic=test_mnemonic, password='TREZOR') == test_seed
