37b9650d1938990a76a93e6784f6fe2d56cdb3de
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-05-12 16:08:49 +0800

    Add script tests CI jobs

--- test_deposit_script.py
+++ test_deposit_script.py
@@ -1,48 +1,11 @@
 import asyncio
 import os
 
-import pytest
-
-from click.testing import CliRunner
-
-from eth2deposit import deposit
-from eth2deposit.deposit import main
-from eth2deposit.utils.constants import DEFAULT_VALIDATOR_KEYS_FOLDER_NAME
+# For not importing eth2deposit here
+DEFAULT_VALIDATOR_KEYS_FOLDER_NAME = 'validator_keys'
 
 
-def test_deposit(monkeypatch):
-    # monkeypatch get_mnemonic
-    def get_mnemonic(language, words_path, entropy=None):
-        return "fakephrase"
-
-    monkeypatch.setattr(deposit, "get_mnemonic", get_mnemonic)
-
-    # Prepare folder
-    my_folder_path = os.path.join(os.getcwd(), 'TESTING_TEMP_FOLDER')
-    if not os.path.exists(my_folder_path):
-        os.mkdir(my_folder_path)
-
-    runner = CliRunner()
-    inputs = ['1', 'english', 'MyPassword', 'MyPassword', 'fakephrase']
-    data = '\n'.join(inputs)
-    result = runner.invoke(main, ['--folder', my_folder_path], input=data)
-
-    assert result.exit_code == 0
-
-    # Check files
-    validator_keys_folder_path = os.path.join(my_folder_path, DEFAULT_VALIDATOR_KEYS_FOLDER_NAME)
-    _, _, key_files = next(os.walk(validator_keys_folder_path))
-    assert len(key_files) == 2
-
-    # Clean up
-    for key_file_name in key_files:
-        os.remove(os.path.join(validator_keys_folder_path, key_file_name))
-    os.rmdir(validator_keys_folder_path)
-    os.rmdir(my_folder_path)
-
-
-@pytest.mark.asyncio
-async def test_script():
+async def main():
     my_folder_path = os.path.join(os.getcwd(), 'TESTING_TEMP_FOLDER')
     if not os.path.exists(my_folder_path):
         os.mkdir(my_folder_path)
@@ -74,6 +37,7 @@ async def test_script():
                 encoded_phrase = seed_phrase.encode()
                 proc.stdin.write(encoded_phrase)
                 proc.stdin.write(b'\n')
+        print(output)
 
     assert len(seed_phrase) > 0
 
@@ -86,3 +50,6 @@ async def test_script():
         os.remove(os.path.join(validator_keys_folder_path, key_file_name))
     os.rmdir(validator_keys_folder_path)
     os.rmdir(my_folder_path)
+
+
+asyncio.run(main())
