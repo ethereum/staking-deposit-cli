c59e89e47dbe9196c882ffd0c099b3a13e663498
Hsiao-Wei Wang <hwwang156@gmail.com>
2020-05-26 16:13:12 +0800

    Add Witti

--- eth2deposit/settings.py
+++ eth2deposit/settings.py
@@ -9,9 +9,18 @@ MainnetSetting = BaseChainSetting(
     GENESIS_FORK_VERSION=bytes.fromhex('00000000'),
 )
 
+
+# Eth2 spec v0.11.3 testnet
+WittiSetting = BaseChainSetting(
+    GENESIS_FORK_VERSION=bytes.fromhex('00000113'),
+)
+
+
 MAINNET = 'mainnet'
+WITTI = 'witti'
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
     MAINNET: MainnetSetting,
+    WITTI: WittiSetting,
 }
 
 
