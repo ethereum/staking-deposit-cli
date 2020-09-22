4a9bab88e6dc17751e68651e9e7cb3647279f381
Carl Beekhuizen <carl@ethereum.org>
2020-09-22 14:29:34 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ THIS IS A STUB FOR RUNNING THE APP
 
 setup(
     name="eth2deposit",
-    version='0.2.1',
+    version='0.3.0',
     py_modules=["eth2deposit"],
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires=">=3.7,<4",
