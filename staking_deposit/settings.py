df7511e488931be22f15eeafd2fb063843988bed
Carl Beekhuizen <carl@ethereum.org>
2022-02-02 15:33:35 -0700

    removes "0x" from to_bytes method

--- staking_deposit/settings.py
+++ staking_deposit/settings.py
@@ -20,9 +20,9 @@ MainnetSetting = BaseChainSetting(NETWORK_NAME=MAINNET, GENESIS_FORK_VERSION=byt
 # Testnet (spec v1.0.1)
 PraterSetting = BaseChainSetting(NETWORK_NAME=PRATER, GENESIS_FORK_VERSION=bytes.fromhex('00001020'))
 # Merge Testnet (spec v1.1.4)
-KintsugiSetting = BaseChainSetting(NETWORK_NAME=KINTSUGI, GENESIS_FORK_VERSION=bytes.fromhex('0x60000069'))
+KintsugiSetting = BaseChainSetting(NETWORK_NAME=KINTSUGI, GENESIS_FORK_VERSION=bytes.fromhex('60000069'))
 # Merge Testnet (spec v1.1.9)
-KilnSetting = BaseChainSetting(NETWORK_NAME=KILN, GENESIS_FORK_VERSION=bytes.fromhex('0x70000069'))
+KilnSetting = BaseChainSetting(NETWORK_NAME=KILN, GENESIS_FORK_VERSION=bytes.fromhex('70000069'))
 
 
 ALL_CHAINS: Dict[str, BaseChainSetting] = {
