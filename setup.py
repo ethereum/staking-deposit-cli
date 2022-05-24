b2993c0a58328bbd5719c53e829b709b2ac3d4e6
Carl Beekhuizen <carl@ethereum.org>
2022-05-24 13:23:43 +0200

    Bumps version number to v2.2.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ THIS IS A STUB FOR RUNNING THE APP
 
 setup(
     name="staking_deposit",
-    version='2.1.0',
+    version='2.2.0',
     py_modules=["staking_deposit"],
     packages=find_packages(exclude=('tests', 'docs')),
     python_requires=">=3.8,<4",
